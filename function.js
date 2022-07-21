0907e46a548e8d667cebbe2b3a96c79352d73dab
akintola4 <akintope830@gmail.com>
2022-07-22 00:14:30 +0100

    u

---
+++ function.js
@@ -0,0 +1,19 @@
+//'functions
+//return with function 
+ package// return sends the returned value back to the caller
+
+function addition(a, b) {
+    var addition= a + b;
+    return addition;
+}
+
+var add = addition(10, 8);
+console.log(add);
+
+function subtraction(a, b) {
+    var subtraction= a - b;
+    return subtraction
+}
+
+var sub = subtraction(20, 10);
+console.log(sub);
