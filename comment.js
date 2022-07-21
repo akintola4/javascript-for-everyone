5f105f9ae3c6bd7431a69262958066d98add882a
akintola4 <akintope830@gmail.com>
2022-07-21 20:38:08 +0100

    node

---
+++ comment.js
@@ -0,0 +1,8 @@
+//how to comment withjs 
+console.log("this is how you comment above for single line comment")
+
+console.log("muti line comment")
+/*
+i can comment for mutiple lines 
+now 
+*/
---
+++ variable.js
@@ -0,0 +1,37 @@
+       //always remmeber to close the line of code with ( ; ) it very important 
+        //working with variables to assign our variables names
+        //js var are case sensitive 
+        //assignment operator (=) use to assign a variable to a value 
+        var firstName ='foster';
+       
+        var number= 77;
+       
+        var _date = 24; //integer
+       
+        var $case = 5.7; //float
+
+        var isFemale = true;
+         
+        var perosn = {};
+
+        var empty= undefined
+        console.log(firstName);
+        console.log(typeof firstName);
+        
+        console.log(number);
+        console.log(typeof number);
+        
+        console.log(_date);
+        console.log(typeof _date);
+        
+        console.log($case);
+        console.log(typeof $case);
+        
+        console.log(isFemale);
+        console.log(typeof isFemale);
+
+        console.log(perosn)
+        console.log(typeof perosn)
+        
+        console.log(empty)
+        console.log(typeof empty)
