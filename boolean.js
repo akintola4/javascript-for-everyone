0ac0521e7580efea412487a93d1fc12e69d34ffd
akintola4 <akintope830@gmail.com>
2022-07-21 22:21:55 +0100

    node work

---
+++ boolean.js
@@ -0,0 +1,17 @@
+//boolean are very useful in alot of use case
+var isTall = true;
+//print out the value
+console.log(isTall);
+
+//using it with an if condition 
+if (isTall) {
+    console.log("Tope is tall.")
+} else {
+    console.log("tope is not tall")
+} 
+
+//also we can flip the value of boolean 
+console.log(!true);
+
+//flip the value of false
+console.log(!false); 
