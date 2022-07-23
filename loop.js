041f0fd9f55b6b7a5e76cd60c430c016c4e4cb2d
akintola4 <akintope830@gmail.com>
2022-07-23 20:34:03 +0100

    up

--- loop.js
+++ loop.js
@@ -1,1 +1,34 @@
 //loops
+//for loop
+
+for (let i = 0; i <=10; i++) {
+    console.log(i);
+    
+}
+
+
+//using a for loop with an array 
+
+var names = [
+    "tope",
+    "tosin",
+    "anna",
+    "kike"
+];
+
+for (let x = 0; x <=names.length; x++) {
+    console.log(names[x]);
+    
+}
+console.log()
+//using forof 
+console.log("for of ")
+for (const name of names) {
+    console.log(name)
+}
+
+//using foreach
+console.log()
+names.forEach(function(name){
+    console.log(name)
+});
---
+++ while.js
@@ -0,0 +1,35 @@
+//while loops with js
+
+var names = [
+    "tope",
+    "fola",
+    "kike",
+    "tosin"
+];
+
+//while loop only works when the condition is true
+
+var test = true;
+
+while (test) {
+    console.log("hello");
+    //the loop will only stop it is false 
+    //if not your loop will run forever
+    test = false;
+}
+
+var index= 0;
+//we can pritn out the elements of an array using while loop
+while (index < names.length ) {
+    console.log(names[index])
+ 
+    index= index + 2; 
+}
+
+//do while
+do {
+    //block of code here priints only once
+    console.log("hello fola");
+    //if true it does any infinite loop
+    //n if false only prints once 
+} while (false);
