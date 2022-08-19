441cf4a3141aa9d45999b57471ab3a0f246a93fb
akintola4 <akintope830@gmail.com>
2022-08-19 11:11:00 +0100

    first node server

---
+++ using_node/module/http/first_server/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const host = '127.0.0.1';
+const port = 5000;
+
+const server = http.createServer((req, res) =>{
+
+});
+
+server.listen(port, host, () => {
+    console.log(`server running at ${host}:${port}`)
+})
