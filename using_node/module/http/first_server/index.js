07f200f0f7e343acc6e9499949ee609b4eaefb47
akintola4 <akintope830@gmail.com>
2022-08-19 12:13:49 +0100

    working with node

--- using_node/module/http/first_server/index.js
+++ using_node/module/http/first_server/index.js
@@ -1,12 +1,23 @@
+//frist server 
+//npm start to start our server 
 const http = require('http');
 
 const host = '127.0.0.1';
 const port = 5000;
 
+//request methods
+// const server = http.createServer((req, res) =>{
+//     console.log(req.method)
+//     console.log(req.url)
+//     console.log(req.headers)
+// });
+
+//go to the json file you will see we have a new depandancies called nodemon
+//it used for monitoring files changes 
+//thus we dont need to restart or run the code {npm start}
 const server = http.createServer((req, res) =>{
-
+   res.end('Hello I am tope akintola')
 });
-
 server.listen(port, host, () => {
     console.log(`server running at ${host}:${port}`)
 })
