32367fb8237cc8aedd894d0be9002bc303710211
akintola4 <akintope830@gmail.com>
2022-09-01 21:21:22 +0100

    new

--- using_node/express/middleware.js
+++ using_node/express/middleware.js
@@ -4,8 +4,15 @@ const express = require ('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res)=>{
-    res.send('Hello,world')
+app.get('/', (req, res, next)=>{
+    res.send('Time:', Date.now())
+});
+
+app.get('/user/id:', (req, res, next)=>{
+    res.send('Request Type:', req.method)
+    next()
+    },(req, res) =>{
+        res.send(res.params.id);
 });
 
 app.listen(port,() =>{
