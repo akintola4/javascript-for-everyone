00ada1a673cf8fbf83611aec6f2f1a510ecd875c
akintola4 <akintope830@gmail.com>
2022-09-30 14:01:18 +0100

    working on reduce

--- data_structure/reduce.js
+++ data_structure/reduce.js
@@ -1,6 +1,18 @@
-import fs from "fs"
+let fs = require('fs')
 var output = fs.readFileSync('pop.txt', 'utf8')
 .trim()
-.split('\n')
+.split("\n")
+.map(line => line.split('\t'))
 
-console.log(output)
+
+// .reduce((users, line)=> {
+// //     users[line[0]] =  []
+// //     users[line[0]].push({
+// //         age: line[1],
+// //         sex: line[2],
+// //         martial: line[3],
+// //     })
+// //     return users
+// // }, {} )
+
+console.log("output", output)
