742d746a47acf79308a83c7ee8c76101e94fe15f
akintola4 <akintope830@gmail.com>
2022-11-25 17:17:09 +0100

    working on a new project

---
+++ projects-with-javascript/swies-form/app.js
@@ -0,0 +1,15 @@
+
+function store() {
+    var key = document.getElementById("key")
+    var email = document.getElementById("email")
+var username = document.getElementById("username")
+var content = document.getElementById("content")
+
+    const report = {
+        email: email.value,
+        username: username.value,
+        content: content.value,
+    }
+
+    window.localStorage.setItem(key, JSON.stringify(report))
+}
