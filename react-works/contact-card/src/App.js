63167058abdbb45e8921ebb4a29fe03669f942ca
akintola4 <akintope830@gmail.com>
2023-02-21 18:08:49 +0100

    learning props

--- react-works/contact-card/src/App.js
+++ react-works/contact-card/src/App.js
@@ -1,13 +1,33 @@
-
-import Card from "./Component/Card";
-import Nav from "./Component/Nav"
-import Hero from "./Component/Hero";
+import Contact from "./Component/Contact";
 export default function App() {
     return(
-        <div className="container">
-            <Nav />
-            <Hero />
-            <Card />
+        <div className="conatainer">
+        <Contact 
+        image= {require("../src/images/cat1.png")}
+        name= "mr. donats"
+        phone= "080607960393"
+        email= "donats.gmail.com"
+        />
+        <Contact 
+        image= {require("../src/images/cat2.png")}
+        name= "mr. james"
+        phone= "080679604393"
+        email= "james.gmail.com"
+        />
+        <Contact 
+        image= {require("../src/images/cat3.png")}
+        name= "mr. ronal"
+        phone= "080645660393"
+        email= "ronal.gmail.com"
+        />
+        <Contact 
+        image= {require("../src/images/cat4.png")}
+        name= "mr. felic"
+        phone= "081257960393"
+        email= "felic.gmail.com"
+        />
         </div>
+
     )
 }
+
