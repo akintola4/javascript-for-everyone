08349cd8100eb0d0c19b5381929fb8c40e332ad6
akintola4 <akintope830@gmail.com>
2023-02-22 21:43:39 +0100

    finished my assignment

--- react-works/joke-app/src/App.js
+++ react-works/joke-app/src/App.js
@@ -1,33 +1,28 @@
-import Contact from "./Component/Contact";
+import Joke from "./Component/Joke";
 export default function App() {
-    return(
+    return (
         <div className="conatainer">
-        <Contact 
-        image= {require("../src/images/cat1.png")}
-        name= "mr. donats"
-        phone= "080607960393"
-        email= "donats.gmail.com"
-        />
-        <Contact 
-        image= {require("../src/images/cat2.png")}
-        name= "mr. james"
-        phone= "080679604393"
-        email= "james.gmail.com"
-        />
-        <Contact 
-        image= {require("../src/images/cat3.png")}
-        name= "mr. ronal"
-        phone= "080645660393"
-        email= "ronal.gmail.com"
-        />
-        <Contact 
-        image= {require("../src/images/cat4.png")}
-        name= "mr. felic"
-        phone= "081257960393"
-        email= "felic.gmail.com"
-        />
+            <Joke
+                setup="I got my daughter a fridge for her birthday."
+                punchline="I can't wait to see her face light up when she opens it."
+
+            />
+            <Joke
+
+                setup="How did the hacker escape the police?"
+                punchline="He just ransomware!"
+            />
+            <Joke
+
+                setup="Why don't pirates travel on mountain roads?"
+                punchline="Scurvy."
+            />
+            <Joke
+
+                setup="Why do bees stay in the hive in the winter?"
+                punchline="Swarm."
+            />
         </div>
 
     )
 }
-
