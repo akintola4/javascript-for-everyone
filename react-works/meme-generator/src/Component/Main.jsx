c407292a66e506b8b4b1a0f95c647b581746dca7
akintola4 <akintope830@gmail.com>
2023-03-09 10:19:53 +0100

    added new component

---
+++ react-works/meme-generator/src/Component/Main.jsx
@@ -0,0 +1,12 @@
+export default function Main(props) {
+    return(
+        <div className="container">
+            <div className="input-section">
+            <input type="text" className="first-input" />
+            <input type="text" className="second-input"  />
+            <input type="button" value="" className="submit"  />
+            </div>
+            <img src="" alt="" />
+        </div>
+    )
+}
---
+++ react-works/meme-generator/src/Component/Nav.jsx
@@ -0,0 +1,14 @@
+export default function Nav() {
+    return (
+        <nav className="navigation">
+            
+                <div className="logoSection">
+                    <img className="logo" src="" alt="react logo" />
+                    <h2 className="logoName">Made with React</h2>
+                </div>
+                <div>
+                    <h3 className="nav-items">Meme generator</h3>
+                </div>
+        </nav>
+    )
+}
