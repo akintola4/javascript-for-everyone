81a98aede9d0f2c633629a69b087e348494347ad
akintola4 <akintope830@gmail.com>
2023-03-10 16:13:14 +0100

    working on the event listener

--- react-works/meme-generator/src/Component/Main.jsx
+++ react-works/meme-generator/src/Component/Main.jsx
@@ -1,4 +1,7 @@
 export default function Main(props) {
+    function click(){
+  alert("it was clicked")
+    }
     return (
         <main className="container">
             <form className="input-section">
@@ -7,7 +10,11 @@ export default function Main(props) {
                     <input type="text" className="input" />
                 </div>
                 <div className="input-submit">
-                    <input className="submit" type="button" value="Get a new meme image  🖼"  />
+                    {/* instead of using the normal method to call which is click()
+                    we are going to just call it like a value click without the () 
+                    reason why is for it not to load immediate but wait till it has been clicked
+                    */}
+                    <input onClick={click} className="submit" type="button" value="Get a new meme image  🖼"  />
                 </div>
             </form>
             <img className="img" src="/memeimg.png" alt="" />
